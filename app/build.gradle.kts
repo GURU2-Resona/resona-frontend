1958ddb49bd6b16ff73955f8f5a882ac3e69d5a3
yangjiae12 <yangjiae12@gmail.com>
2026-01-21 17:00:44 +0900

    chore: 의존성 주석 수정 #2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,20 +44,19 @@ android {
 }
 
 dependencies {
-    // 1. 문제의 핵심: core-ktx 버전을 1.12.0으로 강제 고정합니다. (34버전용)
+    // core-ktx 버전 고정
     implementation("androidx.core:core-ktx:1.12.0")
 
-    // 2. 네비게이션 라이브러리
+    // nav
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
-    // 3. 나머지 기본 라이브러리 (중복 제거됨)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
-    // 테스트 라이브러리
+    // test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
