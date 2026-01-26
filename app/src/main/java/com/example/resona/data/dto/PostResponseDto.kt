7fb33379b96d3a005280e5afe5eeb3573ab0f37e
yangjiae12 <yangjiae12@gmail.com>
2026-01-26 16:35:59 +0900

    feat: 서버 응답값 변경에 따른 연동 로직 수정 #24

--- app/src/main/java/com/example/resona/data/dto/PostResponseDto.kt
+++ app/src/main/java/com/example/resona/data/dto/PostResponseDto.kt
@@ -6,7 +6,7 @@ data class PostResponseDto(
     @SerializedName("postId") val postId: Int,
     @SerializedName("title") val title: String,
     @SerializedName("songTitle") val songTitle: String,
-    @SerializedName("albumImage") val albumImage: String,
+    @SerializedName("songUrl") val songUrl: String,
     @SerializedName("writerProfileImage") val writerProfileImage: String?, // Nullable로 변경
     @SerializedName("writerNickname") val writerNickname: String
 )
